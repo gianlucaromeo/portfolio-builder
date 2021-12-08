e2471926c2819f4a71c2786b59adec6cbd9f0183
gianlucaromeo <gianlucaromeo@outlook.com>
2021-12-08 17:13:18 +0100

    project setup
    added initial classes and bootstrap, css, js templates

---
+++ portfolio-builder-prova/src/test/java/it/unical/demacs/portfoliobuilder/PortfolioBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.demacs.portfoliobuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
