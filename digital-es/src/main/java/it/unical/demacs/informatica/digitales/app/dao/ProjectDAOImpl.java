c6eb0bff0d2b85d28660ad574909b7710ca825f6
gianlucaromeo <gianlucaromeo@outlook.com>
2021-12-15 15:49:51 +0100

    fixed IMPL classes

--- digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dao/ProjectDAOImpl.java
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dao/ProjectDAOImpl.java
@@ -9,23 +9,35 @@ import it.unical.demacs.informatica.digitales.app.database.DBUtil;
 import it.unical.demacs.informatica.digitales.app.database.protocol.Protocol;
 
 public class ProjectDAOImpl extends DAOImpl implements DAO<Project>{
+	
+	private static ProjectDAOImpl instance = null;
+	
+	public static ProjectDAOImpl getInstance() {
+		if (instance == null) {
+			instance = new ProjectDAOImpl();
+		}
+		return instance;
+	}
+	
+	private ProjectDAOImpl() {
+	}
+	
 	@Override
 	public String create(Project project) {
 
 		con = DBUtil.getInstance().getConnection();
 		
-		String query = "INSERT INTO projects VALUES(?,?,?,?,?,?);";
+		String query = "INSERT INTO projects VALUES(DEFAULT,?,?,?,?,?);";
 		
 		try {
 			
 			p = con.prepareStatement(query);
 			
-			p.setNull(1, Types.INTEGER);
-			p.setLong(2,project.getId());
-			p.setString(3,project.getTitle());
-			p.setString(4, project.getDescription());
-			p.setString(5, project.getPicture());
-			p.setString(6, project.getLinkRef());
+			p.setLong(1,project.getId());
+			p.setString(2,project.getTitle());
+			p.setString(3, project.getDescription());
+			p.setString(4, project.getPicture());
+			p.setString(5, project.getLinkRef());
 			
 			p.executeUpdate(query);
 			
