56d78e6eae27375f574ecdf760a8087212f50cfd
cristiandrami <cristiandramisino@libero.it>
2021-12-15 15:51:10 +0100

    [cristian] singleton and query fixing added on curriculum classes and UserMainInformation

--- digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dao/UserMainInformationsDAOImpl.java
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dao/UserMainInformationsDAOImpl.java
@@ -8,36 +8,45 @@ import it.unical.demacs.informatica.digitales.app.database.DBUtil;
 import it.unical.demacs.informatica.digitales.app.database.protocol.Protocol;
 
 public class UserMainInformationsDAOImpl extends DAOImpl implements DAO<UserMainInformations> {
+	private static UserMainInformationsDAOImpl instance= null;
+	
+	private UserMainInformationsDAOImpl() {}
+	
+	public static UserMainInformationsDAOImpl getInstance(){
+		if(instance==null)
+			instance= new UserMainInformationsDAOImpl();
+		return instance;
+	}
 
 	@Override
 	public String create(UserMainInformations userMainInfo) {
 		con = DBUtil.getInstance().getConnection();
 		
-		String query = "INSERT INTO users_main_informations VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
+		String query = "INSERT INTO users_main_informations VALUES(DEFAULT,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);";
 		
 		try {
 			
 			p = con.prepareStatement(query);
 			
-			p.setNull(1, Types.INTEGER);
-			p.setString(2, userMainInfo.getProfilePicture());
-			p.setString(3, userMainInfo.getLogoPicture());
-			p.setString(4, userMainInfo.getLogoName());
-			p.setString(5, userMainInfo.getBio());
-			p.setString(6, userMainInfo.getPresentationPicture1());
-			p.setString(7, userMainInfo.getPresentationPicture2());
-			p.setString(8, userMainInfo.getPresentationPicture3());
-			p.setString(9, userMainInfo.getSpecialSkillName1());
-			p.setString(10, userMainInfo.getSpecialSkillName2());
-			p.setString(11, userMainInfo.getSpecialSkillName3());
-			p.setString(12, userMainInfo.getSpecialSkillDescr1());
-			p.setString(13, userMainInfo.getSpecialSkillDescr2());
-			p.setString(14, userMainInfo.getSpecialSkillDescr3());
-			p.setString(15, userMainInfo.getFacebookLinkRef());
-			p.setString(16, userMainInfo.getInstagramLinkRef());
-			p.setString(17, userMainInfo.getTwitterLinkRef());
+		
+			p.setString(1, userMainInfo.getProfilePicture());
+			p.setString(2, userMainInfo.getLogoPicture());
+			p.setString(3, userMainInfo.getLogoName());
+			p.setString(4, userMainInfo.getBio());
+			p.setString(5, userMainInfo.getPresentationPicture1());
+			p.setString(6, userMainInfo.getPresentationPicture2());
+			p.setString(7, userMainInfo.getPresentationPicture3());
+			p.setString(8, userMainInfo.getSpecialSkillName1());
+			p.setString(9, userMainInfo.getSpecialSkillName2());
+			p.setString(10, userMainInfo.getSpecialSkillName3());
+			p.setString(11, userMainInfo.getSpecialSkillDescr1());
+			p.setString(12, userMainInfo.getSpecialSkillDescr2());
+			p.setString(13, userMainInfo.getSpecialSkillDescr3());
+			p.setString(14, userMainInfo.getFacebookLinkRef());
+			p.setString(15, userMainInfo.getInstagramLinkRef());
+			p.setString(16, userMainInfo.getTwitterLinkRef());
 			
-			p.executeUpdate(query);
+			p.executeUpdate();
 			
 		} catch (SQLException e) {
 			System.err.println("[UserMainInformationsDAOImpl] [create]: ");
