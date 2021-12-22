dab41b79777617f504609000b07ed7b32b497358
cristiandrami <cristiandramisino@libero.it>
2021-12-22 10:24:56 +0100

    dashboard added expression language

--- digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dashboard/AdminPageRedirectController.java
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dashboard/AdminPageRedirectController.java
@@ -3,6 +3,7 @@ package it.unical.demacs.informatica.digitales.app.dashboard;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -14,6 +15,11 @@ public class AdminPageRedirectController {
 	@PostMapping("/dashboard/{username}/admin-page")
 	public String goToUserAdminPage(@PathVariable String username, HttpServletRequest req) {
 		req.setAttribute("username", username);
+		return "admin_page";
+	}
+	@GetMapping("/dashboard/admin-page")
+	public String goToUserAdminPageGet( HttpServletRequest req) {
+	//	req.setAttribute("username", username);
 		return "admin_page";
 	}
 	
