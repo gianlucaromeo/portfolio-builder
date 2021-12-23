2ee45fb12bc5b20999a1460d5ced3b03cd6da184
gianlucaromeo <gianlucaromeo@outlook.com>
2021-12-23 10:48:28 +0100

    Update AdminPageRedirectController
    Added RequestMapping to the class.

--- digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dashboard/AdminPageRedirectController.java
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dashboard/AdminPageRedirectController.java
@@ -6,18 +6,20 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.unical.demacs.informatica.digitales.app.dao.UserDAOImpl;
 
 @Controller
+@RequestMapping("/dashboard")
 public class AdminPageRedirectController {
 
-	@PostMapping("/dashboard/{username}/admin-page")
+	@PostMapping("/{username}/admin-page")
 	public String goToUserAdminPage(@PathVariable String username, HttpServletRequest req) {
 		req.setAttribute("username", username);
 		return "admin_page";
 	}
-	@GetMapping("/dashboard/admin-page")
+	@GetMapping("/admin-page")
 	public String goToUserAdminPageGet( HttpServletRequest req) {
 	//	req.setAttribute("username", username);
 		return "admin_page";
