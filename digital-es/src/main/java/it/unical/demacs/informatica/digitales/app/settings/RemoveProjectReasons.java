8ec94a998445848e8bf74c48937c07f43ded43e8
gianlucaromeo <gianlucaromeo@outlook.com>
2022-01-05 19:18:33 +0100

    Implemented Moderator Projects. (To finish)
    Still need to add js controls when click on "Remove"

---
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/settings/RemoveProjectReasons.java
@@ -0,0 +1,8 @@
+package it.unical.demacs.informatica.digitales.app.settings;
+
+public class RemoveProjectReasons {
+
+	public static final String INAPPROPRIATE_CONTENT = "inappropriate content";
+
+	
+}
