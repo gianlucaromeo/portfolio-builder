0091383b902f6cd8c1a112f3b565e205996de956
cristiandrami <cristiandramisino@libero.it>
2022-01-06 16:29:30 +0100

    restore

--- digital-es/src/main/webapp/dashboard_assets/js/moderatorPosts.js
+++ digital-es/src/main/webapp/dashboard_assets/js/moderatorPosts.js
@@ -1,226 +1,267 @@
-var removeProjectReasons = [];
+/**
+ * 
+ */
+var banReasons=[];
 
-var allUsers = {};
-var allProfilePictures = {};
 
-var selectedUserId = 8;
-var selectedUserProjects = {};
-
-function fetchReasons() {
-
-	console.log("Fetching reasons..");
-
-	$.ajax({
-
-		url: "/get_remove_projects_reasons",
-		contentType: "application/json",
-		type: "post",
-		data: JSON.stringify(selectedUserId),
-		dataType: "json",
-
-	}).done(function(reasons) {
-
-		removeProjectReasons = reasons;
-		console.log("Reasons fetched.")
-		console.log(reasons);
-
-	});
-
-}
-
-function fetchAllUsers() {
-
-	console.log("Fetching users..")
-
+function start(){
+	getBanReasons();
+	
+	
+	$("#postsContainer").hide();
 	$.ajax({
 
 		url: "/get_users_data_action",
 		contentType: "application/json",
 		type: "post",
-		data: JSON.stringify(selectedUserId),
 		dataType: "json",
 
-	}).done(function(users) {
+	}).done(function(data) {
+		//console.log(data);
+		users = data;
 
-		allUsers = users;
-		allUsers.forEach(user => buildUserRow(user));
-		allUsers.forEach(user => fetchUserProfilePicture(user.id));
-
-	});
-
-}
-
-function fetchUserProjects() {
-
-	if (selectedUserId < 0) {
-		console.log("Invalid user id.");
-		return;
-	}
-
-	$.ajax({
-
-		url: "/get_projects_by_user_id",
-		contentType: "application/json",
-		type: "post",
-		data: JSON.stringify(selectedUserId),
-		dataType: "json",
-
-	}).done(function(selectedUserProjects) {
-
-		clearProjectsTable();
-
-		if (selectedUserProjects.length == 0) {
-			console.log("Users has no projects.");
-		} else {
-			console.log("User has " + selectedUserProjects.length + " projects. Building..")
-			selectedUserProjects.forEach(project => buildProjectRow(project));
-			console.log("User's projects built.")
+		if (users.length !== 0) {
+			//console.log("no data");
+		
+			users.forEach(user => {
+				addUserOnTable(user);
+				//console.log(user);
+			});
 		}
 
 	});
-
 }
 
-function fetchUserProfilePicture(userId) {
+function addUserOnTable(user){
+	$("#usersTableBody").append(createUserRow(user));
+	setUserTableUserImage(user.id);
+	setEventOnUserRow(user.id);
+}
 
+function createUserRow(userData){
+	//console.log(userData);
+		return `<tr id="rowUserId${userData.id}">
+				<td><img class="rounded-circle me-2" width="30" height="30"
+					src="undefined" id="profileImageUserId${userData.id}">${userData.firstName} ${userData.lastName} </td>
+				<td>${userData.email}</td>
+				<td>${userData.dateOfBirth}</td>
+				<td>${userData.signUpDate}</td>
+			</tr>`;
+}
+
+function setUserTableUserImage(id){
 	$.ajax({
 
 		url: "/get_user_profile_image",
 		contentType: "application/json",
 		type: "post",
-		data: JSON.stringify(userId),
+		data: JSON.stringify(id),
 		dataType: "json",
 
-	}).done(function(userProfilePicture) {
-		$("#profilePicture_" + userId).attr("src", userProfilePicture);
+	}).done(function(profileImage) {
+		//console.log(data);
+		//console.log($("#profileImageUserId"+id));
+			
+		if(profileImage!=="error"){
+			$("#profileImageUserId"+id).attr("src", profileImage);
+		}
+			
+
 	});
-
+	
+	
 }
 
-function clearProjectsTable() {
-	$("#projectsTableBody tr").remove();
-}
-
-function clearUsersTable() {
-	$("#usersTableBody").remove();
-}
-
-
-/* ==================================== ROWS =================================== */
-
-function buildRemoveBtn(projectId) {
-	return `<!-- Button trigger modal -->
-			<button type="button" class="btn btn-danger btn-sm"
-				data-bs-toggle="modal" data-bs-target="#modal_${projectId}"
-				id="removeProjectBtn_${projectId}" style="margin-bottom: 10px">Delete</button>
-			<!-- Modal -->
-			<div class="modal fade" id="modal_${projectId}" tabindex="-1"
-				aria-labelledby="modalLabel" aria-hidden="true">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title" id="modalLabel">Delete</h5>
-							<button type="button" class="btn-close" data-bs-dismiss="modal"
-								aria-label="Close"></button>
-						</div>
-						<div class="modal-body">Select the reason why You want to remove this Project.</div>
-						<div id="selectDiv">
-						<select name="reasons" id="reasonsSelect_${projectId}">
-						</select>
-						<div class="modal-footer">
-							<button type="button" class="btn btn-secondary"
-								data-bs-dismiss="modal"">Close</button>
-							<button type="button" class="btn btn-danger" id="confirmRemove_${projectId}"
-								data-bs-dismiss="modal">Delete</button>
-						</div>
-					</div>
-				</div>
-			</div>`;
-}
-
-function buildProjectRow(project) {
-
-	row = `<tr class="shadow-sm p-3 mb-5" id="project_${project.id}">
-				<td class="align-middle"><img class=" me-2" width="100" height="50"
-					src="${project.picture}" id="projectPicture_${project.id}"></td>
-				<td class="align-middle"><h5>${project.title}<h5></td>
-				<td class="align-middle">${project.description}</td>
-				<td class="align-middle">${buildRemoveBtn(project.id)}</td>
-			</tr>`;
-
-	$("#projectsTableBody").append(row);
-	addClickOnConfirmRemoveProjectBtn(project.id);
-	removeProjectReasons.forEach(reason => addReasonToPopup(project.id, reason));
-
-}
-
-function addReasonToPopup(projectId, reason) {
-	$("#reasonsSelect_" + projectId).append(`<option value="${reason}">${reason}</option>`);
-}
-
-function addClickOnConfirmRemoveProjectBtn(projectId) {
-	$("#confirmRemove_" + projectId).click(function() {
-
-		selectedReason = $("#reasonsSelect_" + projectId + " option:selected").val();
-		var removeProjectRequest = {
-			id: projectId,
-			reason: selectedReason
-		};
-
-		// Send data to Server. Action: Remove Project
+function setEventOnUserRow(id){
+	$("#rowUserId"+id).click(function() {
+		$("#postsSection").empty();
 		$.ajax({
 
-			url: "/remove_user_project",
+			url: "/get_users_posts_by_id_not_banned",
 			contentType: "application/json",
 			type: "post",
-			data: JSON.stringify(removeProjectRequest),
+			data: JSON.stringify(id),
 			dataType: "json",
 
-		}).done(function(status) {
-			if (status == "ok") {
-				console.log("Project removed.")
+		}).done(function(data) {
+			//console.log(data);
+			posts = data;
+			//console.log(posts);
+			if (posts.length === 0) {
+				
 			} else {
-				console.log("Remove project status error: " + status);
+				posts.forEach(post => {
+					addUserPost(post);
+					//console.log(post);
+				});
 			}
-			fetchUserProjects();
+
+		});
+		
+		
+		$("#postsContainer").show();
+
+	});
+}
+
+function addUserPost(post) {
+	prependPost(post);
+	if(post.picture==="undefined"){
+		$("#imageId"+post.id).hide();
+		
+	}
+	
+	setBanReasonsOnDelete(post.id);
+	setEventOnConfirmBan(post.id);
+	//setEventOnDelete(post.id);
+}
+
+function setEventOnConfirmBan(id){
+	let selectedReason= $('#reasonsSelectId'+id+' option:selected').val();
+	var banRequest={
+		postId: id,
+		reason: selectedReason
+	}
+	$("#confirmDelete" + id).click(function(e) {
+		e.preventDefault();
+		console.log(id);
+		$.ajax({
+
+			url: "/ban_post",
+			contentType: "application/json",
+			data: JSON.stringify(banRequest),
+			type: "post",
+			dataType: "json",
+
+		}).done(function(id) {
+			$("#post" + id).remove();
 		});
 
-		//console.log("Remove project. Id: " + removeProjectRequest.id + ". Reason: " + removeProjectRequest.reason);
+
+		//$("#modalId"+id).modal('toggle');
+
+	});
+	
+}
+function setBanReasonsOnDelete(id){
+	banReasons.forEach(function(reason) {
+		//console.log(reason);
+		appendReason(reason, id);
+		
+		//console.log(reason);
 	});
 }
 
-function addClickOnRow(userId) {
-	$("#tr_user_" + userId).click(function() {
-		selectedUserId = userId;
-		console.log("Clicked on userid: " + userId);
-		fetchUserProjects();
+function prependPost(post) {
+	$("#postsSection").prepend(createPost(post));
+}
+
+function createPost(post){
+	let id = post.id;
+	let title = post.title;
+	let description = post.description;
+	let imagePath = post.picture;
+	let publicationDate = post.pubblicationDate;
+	let lastEditDate = post.lastEditDate;
+	let refLink = post.refLink;
+	
+
+	//console.log(post.pubblicationDate);
+	return `<div  id="post${id}" class="row">
+			<div class="postButtonDiv">
+								<div class="text-left mb-3" style="text-align: right;">
+									`+ getDeleteButton(id) + `
+			</div>
+			<div class="col-md-12 col-lg-12">
+				<div class="card border-0">
+					<img class="card-img-top scale-on-hover"
+						src="${imagePath}"
+						alt="Card Image" height="350" id="imageId${id}">
+					<div style="text-align: center; margin-top: 10px ">
+						<h5><strong>
+							Title: ${title}
+							</strong>
+						</h5>
+						<p class="text-muted card-text">Description: ${description} </p>
+						
+						Reference Link: <a href="${refLink}">${refLink}</a>
+					</div>
+				
+					<div style="text-align: left; margin-top: 10px ">
+						<h6 id="publicationDateLabelId${id}">Publication date:
+							${publicationDate}</h6>
+						<h6 id="lastEditDateLabelId${id}">Last Edit date:
+							${lastEditDate}</h6>
+					</div>
+				</div>
+			</div>
+
+				
+								
+			</div> <hr/>`;
+}
+
+function getDeleteButton(id) {
+	var deleteButton = `<!-- Button trigger modal -->
+						<button type="button" class="btn btn-danger btn-sm"
+							data-bs-toggle="modal" data-bs-target="#modalId${id}"
+							id="deleteId${id}" style="margin-bottom: 10px">Delete</button>
+						<!-- Modal -->
+						<div class="modal fade" id="modalId${id}" tabindex="-1"
+							aria-labelledby="modalLabel" aria-hidden="true">
+							<div class="modal-dialog">
+								<div class="modal-content">
+									<div class="modal-header">
+										<h5 class="modal-title" id="modalLabel">Delete</h5>
+										<button type="button" class="btn-close" data-bs-dismiss="modal"
+											aria-label="Close"></button>
+									</div>
+									<div class="modal-body">Please give a reson why you want to delete this post...</div>
+									<div id="selectDiv">
+									<select name="reasons" id="reasonsSelectId${id}">
+									</select>
+									<div class="modal-footer">
+										<button type="button" class="btn btn-secondary"
+											data-bs-dismiss="modal"">Close</button>
+										<button type="button" class="btn btn-danger" id="confirmDelete${id}"
+											data-bs-dismiss="modal">Delete</button>
+									</div>
+								</div>
+							</div>
+						</div>`;
+
+	//console.log(banReasons);
+	
+	
+	return deleteButton;
+}
+function appendReason(reason, id){
+	$("#reasonsSelectId" + id).append(`<option value="${reason}">${reason}</option>`);
+	//console.log($("#reasonsSelectId" + id).val();
+}
+function getBanReasons(){
+	$.ajax({
+
+		url: "/get_ban_reasons",
+		contentType: "application/json",
+		type: "post",
+		
+
+	}).done(function(data) {
+		reasons=JSON.parse(data);
+		//console.log(reasons);
+		if (reasons.length !== 0) {
+			console.log("ciao")
+			setBanReasons(reasons);	
+		} 
+
 	});
 }
 
-function buildUserRow(user) {
-
-	clearUsersTable();
-
-	row = `<tr class="shadow-sm p-3 mb-5 moderator-projects-user" id="tr_user_${user.id}">
-				<td><img class="rounded-circle me-2" width="30" height="30"
-					src="" id="profilePicture_${user.id}">${user.username}</td>
-			</tr>`;
-
-	$("#allUsersTableBody").append(row);
-	addClickOnRow(user.id);
-	//console.log("Row appended to tbody. Username: " + user.username);
-
+function setBanReasons(reasons){
+	banReasons=reasons;
+//	console.log(banReasons);
 }
 
-/* =================================== END ROWS ======================================= */
 
-
-window.onload = (event) => {
-	console.log("Load page Projects for Moderators.")
-	fetchReasons();
-	fetchAllUsers();
-};
-
-setTimeout(() => {
-	fetchUserProjects();
-}, 4000)
+start();
