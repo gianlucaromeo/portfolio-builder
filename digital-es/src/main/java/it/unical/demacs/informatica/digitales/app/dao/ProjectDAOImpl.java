a0773e88386e3e22a3d9dd2865d5a657a93a76f6
gianlucaromeo <gianlucaromeo@outlook.com>
2022-01-06 15:44:12 +0100

    Done with Moderator Projects Page

--- digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dao/ProjectDAOImpl.java
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dao/ProjectDAOImpl.java
@@ -1,46 +1,50 @@
 package it.unical.demacs.informatica.digitales.app.dao;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import it.unical.demacs.informatica.digitales.app.beans.Post;
 import it.unical.demacs.informatica.digitales.app.beans.Project;
+import it.unical.demacs.informatica.digitales.app.beans.RemovedProject;
 import it.unical.demacs.informatica.digitales.app.database.DBUtil;
 import it.unical.demacs.informatica.digitales.app.database.protocol.Protocol;
 
-public class ProjectDAOImpl extends DAOImpl implements DAO<Project>{
-	
+public class ProjectDAOImpl extends DAOImpl implements DAO<Project> {
+
 	private static ProjectDAOImpl instance = null;
-	
+
 	public static ProjectDAOImpl getInstance() {
 		if (instance == null) {
 			instance = new ProjectDAOImpl();
 		}
 		return instance;
 	}
-	
+
 	private ProjectDAOImpl() {
 	}
-	
+
 	@Override
 	public String create(Project project) {
 
 		con = DBUtil.getInstance().getConnection();
-		
+
 		String query = "INSERT INTO projects VALUES(DEFAULT,?,?,?,?,?);";
-		
+
 		try {
-			
+
 			p = con.prepareStatement(query);
-			
-			p.setLong(1,project.getUserId());
-			p.setString(2,project.getTitle());
+
+			p.setLong(1, project.getUserId());
+			p.setString(2, project.getTitle());
 			p.setString(3, project.getDescription());
 			p.setString(4, project.getPicture());
 			p.setString(5, project.getLinkRef());
-			
+
 			p.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			System.err.println("[ProjectDAOImpl] [create]: ");
 			e.printStackTrace();
@@ -48,31 +52,31 @@ public class ProjectDAOImpl extends DAOImpl implements DAO<Project>{
 		} finally {
 			closeAll();
 		}
-		
+
 		return Protocol.OK;
-		
+
 	}
 
 	@Override
 	public String update(Project project) {
-		
+
 		con = DBUtil.getInstance().getConnection();
-		
+
 		String query = "UPDATE projects SET user_id=?, title=?, description=?, picture=?, link_ref=? WHERE id=?;";
-		
+
 		try {
-			
+
 			p = con.prepareStatement(query);
-			
-			p.setLong(1,project.getUserId());
-			p.setString(2,project.getTitle());
+
+			p.setLong(1, project.getUserId());
+			p.setString(2, project.getTitle());
 			p.setString(3, project.getDescription());
 			p.setString(4, project.getPicture());
 			p.setString(5, project.getLinkRef());
 			p.setLong(6, project.getId());
-			
+
 			p.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			System.err.println("[ProjectDAOImpl] [update]: ");
 			e.printStackTrace();
@@ -80,33 +84,33 @@ public class ProjectDAOImpl extends DAOImpl implements DAO<Project>{
 		} finally {
 			closeAll();
 		}
-		
+
 		return Protocol.OK;
 	}
-	
+
 	@Override
 	public long findId(Project project) {
 
 		long id = -1; // target
-		
+
 		con = DBUtil.getInstance().getConnection();
-		
+
 		String query = "SELECT id from projects WHERE title=? AND description=? AND user_id=?;";
-		
+
 		try {
-			
+
 			p = con.prepareStatement(query);
-			
+
 			p.setString(1, project.getTitle());
 			p.setString(2, project.getDescription());
 			p.setLong(3, project.getUserId());
-			
+
 			rs = p.executeQuery();
-			
+
 			if (rs.next()) {
 				id = rs.getLong("id");
 			}
-			
+
 		} catch (SQLException e) {
 			System.err.println("[ProjectDAOImpl] [findId]: ");
 			e.printStackTrace();
@@ -114,63 +118,63 @@ public class ProjectDAOImpl extends DAOImpl implements DAO<Project>{
 		} finally {
 			closeAll();
 		}
-		
+
 		return id;
-		
+
 	}
-	
+
 	@Override
 	public Project findById(long id) {
-		
+
 		Project project = null;
-		
+
 		con = DBUtil.getInstance().getConnection();
-		
+
 		String query = "SELECT * FROM projects WHERE id=?;";
-		
+
 		try {
-			
+
 			p = con.prepareStatement(query);
 			p.setLong(1, id);
-			
+
 			rs = p.executeQuery();
-			
+
 			if (rs.next()) {
-				project= new Project();
+				project = new Project();
 				project.setId(id);
 				project.setUserId(rs.getLong("user_id"));
 				project.setTitle(rs.getString("title"));
 				project.setDescription(rs.getString("description"));
 				project.setPicture(rs.getString("picture"));
 				project.setLinkRef(rs.getString("link_ref"));
-			
+
 			}
-	
+
 		} catch (SQLException e) {
 			System.err.println("[ProjectDAOImpl] [findById]: ");
 			e.printStackTrace();
 		} finally {
 			closeAll();
 		}
-		
+
 		return project;
 	}
-	
+
 	@Override
 	public Set<Project> findAll() {
-		
+
 		Set<Project> projects = new HashSet<Project>();
-		
+
 		con = DBUtil.getInstance().getConnection();
-		
+
 		String query = "SELECT * FROM projects;";
 
 		try {
-			
+
 			p = con.prepareStatement(query);
-			
+
 			rs = p.executeQuery();
-			
+
 			while (rs.next()) {
 				Project project = new Project();
 				project.setId(rs.getLong("id"));
@@ -181,25 +185,62 @@ public class ProjectDAOImpl extends DAOImpl implements DAO<Project>{
 				project.setLinkRef(rs.getString("link_ref"));
 				projects.add(project);
 			}
-			
+
 		} catch (SQLException e) {
 			System.err.println("[ProjectDAOImpl] [findAll]: ");
 			e.printStackTrace();
 		} finally {
 			closeAll();
 		}
-		
+
 		return projects;
-		
+
+	}
+
+	public Set<Project> findAllByUserId(Long userId) {
+
+		Set<Project> projects = new HashSet<Project>();
+
+		con = DBUtil.getInstance().getConnection();
+
+		String query = "SELECT * FROM projects WHERE user_id=?;";
+
+		try {
+
+			p = con.prepareStatement(query);
+			p.setLong(1, userId);
+
+			rs = p.executeQuery();
+
+			while (rs.next()) {
+				Project project = new Project();
+				project.setId(rs.getLong("id"));
+				project.setUserId(rs.getLong("user_id"));
+				project.setTitle(rs.getString("title"));
+				project.setDescription(rs.getString("description"));
+				project.setPicture(rs.getString("picture"));
+				project.setLinkRef(rs.getString("link_ref"));
+				projects.add(project);
+			}
+
+		} catch (SQLException e) {
+			System.err.println("[ProjectDAOImpl] [findAllByUserId]: ");
+			e.printStackTrace();
+		} finally {
+			closeAll();
+		}
+
+		return projects;
+
 	}
 	
-public Set<Project> findAllByUserId(Long userId) {
+	public List<Project> findAllByUserIdNotRemoved(long userId) {
 		
-		Set<Project> projects = new HashSet<Project>();
+		List<Project> projects = new ArrayList<Project>();
 		
 		con = DBUtil.getInstance().getConnection();
 		
-		String query = "SELECT * FROM projects WHERE user_id=?;";
+		String query = "SELECT * FROM projects WHERE user_id=? AND id NOT IN (SELECT project_id FROM removed_projects);";
 
 		try {
 			
@@ -220,29 +261,28 @@ public Set<Project> findAllByUserId(Long userId) {
 			}
 			
 		} catch (SQLException e) {
-			System.err.println("[ProjectDAOImpl] [findAllByUserId]: ");
+			System.err.println("[ProjectsDAOImpl] [findAllByUserIdNotRemoved]: ");
 			e.printStackTrace();
 		} finally {
 			closeAll();
 		}
 		
 		return projects;
-		
 	}
 
 	@Override
 	public String delete(Project project) {
 
 		con = DBUtil.getInstance().getConnection();
-		
+
 		String query = "DELETE from projects WHERE id=?";
-		
+
 		try {
-			
+
 			p = con.prepareStatement(query);
 			p.setLong(1, project.getId());
 			p.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			System.err.println("[ProjectDAOImpl] [delete]: ");
 			e.printStackTrace();
@@ -250,9 +290,9 @@ public Set<Project> findAllByUserId(Long userId) {
 		} finally {
 			closeAll();
 		}
-		
+
 		return Protocol.OK;
-		
+
 	}
-	
+
 }
--- digital-es/src/main/webapp/dashboard_assets/js/moderatorProjects.js
+++ digital-es/src/main/webapp/dashboard_assets/js/moderatorProjects.js
@@ -1,15 +1,13 @@
-
-
 var removeProjectReasons = [];
 
-var allUsers = {}; 
+var allUsers = {};
 var allProfilePictures = {};
 
 var selectedUserId = 8;
 var selectedUserProjects = {};
 
 function fetchReasons() {
-	
+
 	console.log("Fetching reasons..");
 
 	$.ajax({
@@ -22,16 +20,16 @@ function fetchReasons() {
 
 	}).done(function(reasons) {
 
-		get_remove_projects_reasons = reasons;
+		removeProjectReasons = reasons;
 		console.log("Reasons fetched.")
 		console.log(reasons);
-		
+
 	});
 
 }
 
 function fetchAllUsers() {
-	
+
 	console.log("Fetching users..")
 
 	$.ajax({
@@ -47,7 +45,7 @@ function fetchAllUsers() {
 		allUsers = users;
 		allUsers.forEach(user => buildUserRow(user));
 		allUsers.forEach(user => fetchUserProfilePicture(user.id));
-		
+
 	});
 
 }
@@ -69,11 +67,12 @@ function fetchUserProjects() {
 
 	}).done(function(selectedUserProjects) {
 
+		clearProjectsTable();
+
 		if (selectedUserProjects.length == 0) {
 			console.log("Users has no projects.");
 		} else {
 			console.log("User has " + selectedUserProjects.length + " projects. Building..")
-			clearProjectsTable();
 			selectedUserProjects.forEach(project => buildProjectRow(project));
 			console.log("User's projects built.")
 		}
@@ -83,7 +82,7 @@ function fetchUserProjects() {
 }
 
 function fetchUserProfilePicture(userId) {
-	
+
 	$.ajax({
 
 		url: "/get_user_profile_image",
@@ -95,6 +94,7 @@ function fetchUserProfilePicture(userId) {
 	}).done(function(userProfilePicture) {
 		$("#profilePicture_" + userId).attr("src", userProfilePicture);
 	});
+
 }
 
 function clearProjectsTable() {
@@ -108,6 +108,36 @@ function clearUsersTable() {
 
 /* ==================================== ROWS =================================== */
 
+function buildRemoveBtn(projectId) {
+	return `<!-- Button trigger modal -->
+			<button type="button" class="btn btn-danger btn-sm"
+				data-bs-toggle="modal" data-bs-target="#modal_${projectId}"
+				id="removeProjectBtn_${projectId}" style="margin-bottom: 10px">Delete</button>
+			<!-- Modal -->
+			<div class="modal fade" id="modal_${projectId}" tabindex="-1"
+				aria-labelledby="modalLabel" aria-hidden="true">
+				<div class="modal-dialog">
+					<div class="modal-content">
+						<div class="modal-header">
+							<h5 class="modal-title" id="modalLabel">Delete</h5>
+							<button type="button" class="btn-close" data-bs-dismiss="modal"
+								aria-label="Close"></button>
+						</div>
+						<div class="modal-body">Select the reason why You want to remove this Project.</div>
+						<div id="selectDiv">
+						<select name="reasons" id="reasonsSelect_${projectId}">
+						</select>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary"
+								data-bs-dismiss="modal"">Close</button>
+							<button type="button" class="btn btn-danger" id="confirmRemove_${projectId}"
+								data-bs-dismiss="modal">Delete</button>
+						</div>
+					</div>
+				</div>
+			</div>`;
+}
+
 function buildProjectRow(project) {
 
 	row = `<tr class="shadow-sm p-3 mb-5" id="project_${project.id}">
@@ -115,23 +145,69 @@ function buildProjectRow(project) {
 					src="${project.picture}" id="projectPicture_${project.id}"></td>
 				<td class="align-middle"><h5>${project.title}<h5></td>
 				<td class="align-middle">${project.description}</td>
-				<td class="align-middle"><button class="btn btn-danger btn-sm" type="submit"
-								id="btnRemoveProject_${project.id}">Remove</button></td>
+				<td class="align-middle">${buildRemoveBtn(project.id)}</td>
 			</tr>`;
 
 	$("#projectsTableBody").append(row);
+	addClickOnConfirmRemoveProjectBtn(project.id);
+	removeProjectReasons.forEach(reason => addReasonToPopup(project.id, reason));
+
+}
+
+function addReasonToPopup(projectId, reason) {
+	$("#reasonsSelect_" + projectId).append(`<option value="${reason}">${reason}</option>`);
+}
+
+function addClickOnConfirmRemoveProjectBtn(projectId) {
+	$("#confirmRemove_" + projectId).click(function() {
+
+		selectedReason = $("#reasonsSelect_" + projectId + " option:selected").val();
+		var removeProjectRequest = {
+			id: projectId,
+			reason: selectedReason
+		};
+
+		// Send data to Server. Action: Remove Project
+		$.ajax({
+
+			url: "/remove_user_project",
+			contentType: "application/json",
+			type: "post",
+			data: JSON.stringify(removeProjectRequest),
+			dataType: "json",
+
+		}).done(function(status) {
+			if (status == "ok") {
+				console.log("Project removed.")
+			} else {
+				console.log("Remove project status error: " + status);
+			}
+			fetchUserProjects();
+		});
+
+		//console.log("Remove project. Id: " + removeProjectRequest.id + ". Reason: " + removeProjectRequest.reason);
+	});
+}
+
+function addClickOnRow(userId) {
+	$("#tr_user_" + userId).click(function() {
+		selectedUserId = userId;
+		console.log("Clicked on userid: " + userId);
+		fetchUserProjects();
+	});
 }
 
 function buildUserRow(user) {
 
 	clearUsersTable();
-	
-	row = `<tr class="shadow-sm p-3 mb-5 moderator-projects-user" id="user_${user.id}">
+
+	row = `<tr class="shadow-sm p-3 mb-5 moderator-projects-user" id="tr_user_${user.id}">
 				<td><img class="rounded-circle me-2" width="30" height="30"
-					src="undefined" id="profilePicture_${user.id}">${user.username}</td>
+					src="" id="profilePicture_${user.id}">${user.username}</td>
 			</tr>`;
 
 	$("#allUsersTableBody").append(row);
+	addClickOnRow(user.id);
 	//console.log("Row appended to tbody. Username: " + user.username);
 
 }
@@ -146,5 +222,5 @@ window.onload = (event) => {
 };
 
 setTimeout(() => {
-  fetchUserProjects();
+	fetchUserProjects();
 }, 4000)
