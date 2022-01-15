b3cc8dedeb4ca9fd89da44aa9232ab7440808fec
cristiandrami <cristiandramisino@libero.it>
2022-01-15 12:21:20 +0100

    posts bug fixed

--- digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dashboard/ErrorRedirect.java
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dashboard/ErrorRedirect.java
@@ -1,7 +1,5 @@
 package it.unical.demacs.informatica.digitales.app.dashboard;
 
-import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,20 +12,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ErrorRedirect implements ErrorController {
 	@RequestMapping("/error")
-	public String handleError(HttpServletRequest request, HttpServletResponse resp) throws IOException {
-		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+	public String handleError(HttpServletRequest request, HttpServletResponse resp) {
+		try {
+			Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
-		if (status != null) {
-	        Integer statusCode = Integer.valueOf(status.toString());
-	        if(statusCode == HttpStatus.NOT_FOUND.value()) {
-	        	resp.sendRedirect("/dashboard/404_page");
-	        }
-	        else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-	        	resp.sendRedirect("/dashboard/500_page");
-	        }
-	    }
-		resp.sendRedirect("/dashboard/404_page");
-		return null;
+			if (status != null) {
+				Integer statusCode = Integer.valueOf(status.toString());
+				if (statusCode == HttpStatus.NOT_FOUND.value()) {
+					resp.sendRedirect("/dashboard/404_page");
+				} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+					resp.sendRedirect("/dashboard/500_page");
+				}
+			}
+
+			resp.sendRedirect("/dashboard/404_page");
+			return null;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 
 }
