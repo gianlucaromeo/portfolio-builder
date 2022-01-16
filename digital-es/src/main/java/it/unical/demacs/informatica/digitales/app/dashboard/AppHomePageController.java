8d3410e12d2bf7220422ea1d02aa8ab3a78258b9
cristiandrami <cristiandramisino@libero.it>
2022-01-16 10:13:05 +0100

    warnings suppressed in controller

--- digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dashboard/AppHomePageController.java
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/dashboard/AppHomePageController.java
@@ -1,10 +1,9 @@
 package it.unical.demacs.informatica.digitales.app.dashboard;
 
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class AppHomePageController {
