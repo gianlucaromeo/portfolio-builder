d7f4288c7dabf88331452a42cbccefdca58631ba
cristiandrami <cristiandramisino@libero.it>
2022-01-16 11:43:36 +0100

    database dump added

--- digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/CreateModeratorsLauncher.java
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/CreateModeratorsLauncher.java
@@ -10,6 +10,7 @@ import it.unical.demacs.informatica.digitales.app.dao.ModeratorDAOImpl;
  */
 public class CreateModeratorsLauncher {
 
+	/* Them are already saved on DataBase*/
 	public static void main(String[] args) {
 		createModerator("moderator1", "moderator1", "moderator1@portfoliobuilder.it");
 		createModerator("moderator2", "moderator2", "moderator2@portfoliobuilder.it");
