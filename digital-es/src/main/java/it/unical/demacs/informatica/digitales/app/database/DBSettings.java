1c7bab416e9f6cbd4e4e8e7351b54dd330281627
cristiandrami <cristiandramisino@libero.it>
2022-01-17 19:22:54 +0100

    test done

--- digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/database/DBSettings.java
+++ digital-es/src/main/java/it/unical/demacs/informatica/digitales/app/database/DBSettings.java
@@ -1,9 +1,7 @@
 package it.unical.demacs.informatica.digitales.app.database;
 
 public class DBSettings {
-
-	public final static String URL = "jdbc:postgresql://localhost:5432/web-project";
+	public final static String URL = "jdbc:postgresql://localhost:5432/web";
 	public final static String USER = "postgres";
 	public final static String PASSWORD = "postgres";
-	
 }
